1e80725e2ea7f5fe53f68451a470eb9c7d9de6c7
unknown <scandiclamar@gmail.com>
2025-03-18 03:55:27 +0000

    addition

--- frontend/src/pages/Auth/VerifyEmail.tsx
+++ frontend/src/pages/Auth/VerifyEmail.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 import { Box, Typography, Paper, Button, CircularProgress, Container } from '@mui/material';
 import axios from 'axios';
-import { AUTH_BASE_URL } from '../../config';
+import { AUTH_BASE_URL } from '../../config.ts';
 
 const VerifyEmail = () => {
   const [searchParams] = useSearchParams();
